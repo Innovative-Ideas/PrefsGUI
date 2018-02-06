9ffa2be4c12fda4756c9511cdda8987ac4a3461a
DAVIS Elliott <davis@team-lab.com>
2018-02-06 13:08:49 +0900

    adjusting / adding PrefsGUI namespace.

--- Assets/Packages/PrefsGUIMenuSystem/Scripts/PrefsGUISettings.cs
+++ Assets/Packages/PrefsGUIMenuSystem/Scripts/PrefsGUISettings.cs
@@ -2,70 +2,74 @@
 using System.Collections;
 using PrefsGUI;
 
-public class PrefsGUISettings : ObjectsInTea.MenuBase //MonoBehaviour
+namespace PrefsGUI
 {
-	public  Prefs.FileLocation fileLocation  = Prefs.FileLocation.PersistantData;
-	public string pathPrefix = "";
 
-	//[System.Serializable]
-	//public class PrefsEnum : PrefsParam<Prefs.FileLocation>
-	//{
-	//	public PrefsEnum( string key, Prefs.FileLocation defaultValue = default( Prefs.FileLocation ) ) : base( key, defaultValue ) { }
-	//}
-	//public PrefsEnum _prefsEnum;
-
-
-	void Awake()
+	public class PrefsGUISettings : PrefsGUI.MenuBase //MonoBehaviour
 	{
-		PrefsGUI.Prefs.SetFileLocation(fileLocation);
-		PrefsGUI.Prefs.SetFilePathPrefix(pathPrefix);
-		// reload file after changing path location
-		PrefsGUI.Prefs.Load();
+		public Prefs.FileLocation fileLocation = Prefs.FileLocation.PersistantData;
+		public string pathPrefix = "";
 
-		//_prefsEnum = new PrefsEnum("Save_Load_Location", fileLocation);
-	}
-
-	//public void OnGUIInternal()
-	public override void OnGUIInternal()
-	{
-
-
-		//if(Prefs.GetFileLocation() != Prefs.FileLocation.NumLocations)
-		//{ 
-		//	var previous = _prefsEnum.Get();
-		//	_prefsEnum.OnGUI("Save/Load Location:");
-		//	// don't let user change values to Prefs.FileLocation.NumLocations
-		//	if(_prefsEnum.Get() == Prefs.FileLocation.NumLocations )
-		//		_prefsEnum.Set(previous);
-
-		//	if ( _prefsEnum.Get() != fileLocation )
-		//	{
-		//		if (GUILayout.Button( "Load Settings File from New Location?"))
-		//		{
-		//			fileLocation = _prefsEnum.Get();
-		//			PrefsGUI.Prefs.SetFileLocation(fileLocation);
-		//			PrefsGUI.Prefs.Load();
-		//		}
-		//	}
+		//[System.Serializable]
+		//public class PrefsEnum : PrefsParam<Prefs.FileLocation>
+		//{
+		//	public PrefsEnum( string key, Prefs.FileLocation defaultValue = default( Prefs.FileLocation ) ) : base( key, defaultValue ) { }
 		//}
-		// pring defaut save location
-		GUILayout.Label(string.Format("Default Save/Load Location: {0}", PrefsGUI.Prefs.GetFileLocation() ));
+		//public PrefsEnum _prefsEnum;
 
-		// save to Streaming Assets
-		if (GUILayout.Button( "SAVE in Streaming Assets & ALL LOCATIONS"))
+
+		void Awake()
 		{
-			PrefsGUI.Prefs.SaveInAllLocations();
-			//PrefsGUI.Prefs.Save();
-		}
-		// load from Streaming Assets
-		if (GUILayout.Button( "LOAD from Streaming Assets"))
-		{
-			PrefsGUI.Prefs.SetFileLocation( Prefs.FileLocation.StreamingAssets );
+			PrefsGUI.Prefs.SetFileLocation(fileLocation);
+			PrefsGUI.Prefs.SetFilePathPrefix(pathPrefix);
+			// reload file after changing path location
 			PrefsGUI.Prefs.Load();
-			PrefsGUI.Prefs.SetFileLocation( fileLocation );
+
+			//_prefsEnum = new PrefsEnum("Save_Load_Location", fileLocation);
 		}
 
-		base.OnGUIInternal();
-	}
+		//public void OnGUIInternal()
+		public override void OnGUIInternal()
+		{
 
-}
+
+			//if(Prefs.GetFileLocation() != Prefs.FileLocation.NumLocations)
+			//{ 
+			//	var previous = _prefsEnum.Get();
+			//	_prefsEnum.OnGUI("Save/Load Location:");
+			//	// don't let user change values to Prefs.FileLocation.NumLocations
+			//	if(_prefsEnum.Get() == Prefs.FileLocation.NumLocations )
+			//		_prefsEnum.Set(previous);
+
+			//	if ( _prefsEnum.Get() != fileLocation )
+			//	{
+			//		if (GUILayout.Button( "Load Settings File from New Location?"))
+			//		{
+			//			fileLocation = _prefsEnum.Get();
+			//			PrefsGUI.Prefs.SetFileLocation(fileLocation);
+			//			PrefsGUI.Prefs.Load();
+			//		}
+			//	}
+			//}
+			// pring defaut save location
+			GUILayout.Label(string.Format("Default Save/Load Location: {0}", PrefsGUI.Prefs.GetFileLocation()));
+
+			// save to Streaming Assets
+			if (GUILayout.Button("SAVE in Streaming Assets & ALL LOCATIONS"))
+			{
+				PrefsGUI.Prefs.SaveInAllLocations();
+				//PrefsGUI.Prefs.Save();
+			}
+			// load from Streaming Assets
+			if (GUILayout.Button("LOAD from Streaming Assets"))
+			{
+				PrefsGUI.Prefs.SetFileLocation(Prefs.FileLocation.StreamingAssets);
+				PrefsGUI.Prefs.Load();
+				PrefsGUI.Prefs.SetFileLocation(fileLocation);
+			}
+
+			base.OnGUIInternal();
+		}
+
+	} // end class
+} // end namespace
