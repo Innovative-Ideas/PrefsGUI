8c1c8972d1e86514b416e9c069fdd222f030cdc8
DAVIS Elliott <davis@team-lab.com>
2018-02-09 16:20:03 +0900

    1. Listing all setting files date/time stamp. 2. Tracking last setting file loaded. 3. Making Load from alternate location button work regardless of what default load location is.

--- Assets/Packages/PrefsGUIMenuSystem/Scripts/PrefsGUISettings.cs
+++ Assets/Packages/PrefsGUIMenuSystem/Scripts/PrefsGUISettings.cs
@@ -7,8 +7,10 @@ namespace PrefsGUI
 
 	public class PrefsGUISettings : PrefsGUI.MenuBase //MonoBehaviour
 	{
+		[Tooltip("Default File Save/Load location")]
 		public Prefs.FileLocation fileLocation = Prefs.FileLocation.PersistantData;
 		public string pathPrefix = "";
+		private Prefs.FileLocation fileLastLoadedFrom = Prefs.FileLocation.PersistantData;
 
 		//[System.Serializable]
 		//public class PrefsEnum : PrefsParam<Prefs.FileLocation>
@@ -22,8 +24,23 @@ namespace PrefsGUI
 		{
 			PrefsGUI.Prefs.SetFileLocation(fileLocation);
 			PrefsGUI.Prefs.SetFilePathPrefix(pathPrefix);
+
+			/*
+			// work in progress
+		//#if (UNITY_EDITOR == false)
+			// if file location is streaming assets, change it to persistant data
+			if (fileLocation == Prefs.FileLocation.StreamingAssets)
+			{
+				System.DateTime defaultFileDT = PrefsGUI.Prefs.GetFileTimeStamp();
+
+				// if fileLocationno persistant data file exists, copy latest streaming assets file to persistant data
+				// if persistant data file is older than streaming assets file, copy latest streaming assets file to persistant data
+			}
+		//#endif
+		*/
+
 			// reload file after changing path location
-			PrefsGUI.Prefs.Load();
+			PrefsGUILoad();
 
 			//_prefsEnum = new PrefsEnum("Save_Load_Location", fileLocation);
 		}
@@ -31,28 +48,18 @@ namespace PrefsGUI
 		//public void OnGUIInternal()
 		public override void OnGUIInternal()
 		{
-
-
-			//if(Prefs.GetFileLocation() != Prefs.FileLocation.NumLocations)
-			//{ 
-			//	var previous = _prefsEnum.Get();
-			//	_prefsEnum.OnGUI("Save/Load Location:");
-			//	// don't let user change values to Prefs.FileLocation.NumLocations
-			//	if(_prefsEnum.Get() == Prefs.FileLocation.NumLocations )
-			//		_prefsEnum.Set(previous);
-
-			//	if ( _prefsEnum.Get() != fileLocation )
-			//	{
-			//		if (GUILayout.Button( "Load Settings File from New Location?"))
-			//		{
-			//			fileLocation = _prefsEnum.Get();
-			//			PrefsGUI.Prefs.SetFileLocation(fileLocation);
-			//			PrefsGUI.Prefs.Load();
-			//		}
-			//	}
-			//}
 			// pring defaut save location
-			GUILayout.Label(string.Format("Default Save/Load Location: {0}", PrefsGUI.Prefs.GetFileLocation()));
+
+			GUILayout.Label(string.Format("Default Save/Load Location:\t{0}", PrefsGUI.Prefs.GetFileLocation()), GUILayout.MinWidth(200f));
+			GUILayout.Label("\t(To change 'default Location', change in Unity Editor)"
+				#if (UNITY_EDITOR == false)
+				+ " and rebuild .exe"
+				#endif
+				);
+			
+			GUILayout.Label("");
+			GUILayout.Label(string.Format("File Last Loaded From:\t{0}", fileLastLoadedFrom));
+			GUILayout.Label("");
 
 			// save to Streaming Assets
 			if (GUILayout.Button("SAVE in Streaming Assets & ALL LOCATIONS"))
@@ -60,16 +67,43 @@ namespace PrefsGUI
 				PrefsGUI.Prefs.SaveInAllLocations();
 				//PrefsGUI.Prefs.Save();
 			}
-			// load from Streaming Assets
-			if (GUILayout.Button("LOAD from Streaming Assets"))
+
+			// offer options to load settings files from other locations than the default location
+			for (Prefs.FileLocation i = 0; i < Prefs.FileLocation.NumLocations; ++i)
 			{
-				PrefsGUI.Prefs.SetFileLocation(Prefs.FileLocation.StreamingAssets);
-				PrefsGUI.Prefs.Load();
-				PrefsGUI.Prefs.SetFileLocation(fileLocation);
+				if (i != fileLocation)
+				{ 
+					// provide option to load from stream assets
+					// load from Streaming Assets
+					if (GUILayout.Button(string.Format("LOAD from {0}", i) ))
+					{
+						PrefsGUI.Prefs.SetFileLocation(i);
+						PrefsGUILoad();
+						PrefsGUI.Prefs.SetFileLocation(fileLocation);
+					}
+				}
 			}
+
+			GUILayout.Label("Settings File Timestamps:");
+			for (Prefs.FileLocation i = 0; i < Prefs.FileLocation.NumLocations; ++i)
+			{
+				PrefsGUI.Prefs.SetFileLocation(i);
+				System.DateTime dt = PrefsGUI.Prefs.GetFileTimeStamp();
+				bool validSettingFile = dt.Equals(System.DateTime.MinValue) == false;
+
+				GUILayout.Label(string.Format("{0}:\t {1}", i, validSettingFile ? dt.ToString() : "No File / Unknown"));
+			}
+			PrefsGUI.Prefs.SetFileLocation(fileLocation);
+
 
 			base.OnGUIInternal();
 		}
 
+
+		private void PrefsGUILoad()
+		{
+			PrefsGUI.Prefs.Load();
+			fileLastLoadedFrom = PrefsGUI.Prefs.GetFileLocation();
+		}
 	} // end class
 } // end namespace
