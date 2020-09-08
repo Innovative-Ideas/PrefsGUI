fa17e960276f85ce9302c8faa20fe3c6e8800346
DavisTeamLab <davis@team-lab.com>
2020-09-08 20:23:19 +0900

    Fixing error where client PC would spawn two instances of PrefsGUISync. Instance one was spawned before the client became a client (it was treated as a stand alone computer) Instance two was created via network sync from the server.

--- Runtime/PrefsGUISyncSpawnAtRuntime.cs
+++ Runtime/PrefsGUISyncSpawnAtRuntime.cs
@@ -66,7 +66,7 @@ namespace PrefsGUI
                 return;
 #endif
 
-            if ( SyncNet.isServerOrStandAlone )
+            if ( SyncNet.isServer )
             {
                 SpawnPrefsGUISyncServerOrStandalone();
             }
